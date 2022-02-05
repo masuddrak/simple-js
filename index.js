b7ae933c97878aa3fd56bae77d7406e8b029be51
masudrana <masudranaacb@gmail.com>
2022-02-06 00:43:48 +0600

    search products

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-// recursion
-function recursion(i){
-    if(i<=1){
-        return 1;
+
+let producs=[
+    {name:'iphone', price:200},
+    {name:'symphone phone', price:150},
+    {name:'samsung phone', price:200},
+    {name:'wach w3', price:210},
+    {name:'wach w4', price:2000},
+    {name:'gas lpg', price:1000},
+    {name:'gas plg', price:400},
+]
+function seachProducts(producs,searchText){
+    let seachProduct=[];
+    for(const produc of producs){
+        if(produc.name.includes(searchText)){
+            seachProduct.push(produc);
+        }
     }
-  return i+recursion(--i);
+    return seachProduct;
 }
-console.log(recursion(5));
+console.log(seachProducts(producs,'wach'));
