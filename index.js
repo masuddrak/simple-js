25ab4239c8d4714e0dc1f43043bf48a05c63d352
masudrana <masudranaacb@gmail.com>
2022-02-06 01:01:42 +0600

    filltering price

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 
 let producs=[
-    {name:'iphone', price:200},
-    {name:'symphone phone', price:150},
+    {name:'iphone', price:4000},
+    {name:'symphone phone', price:15000},
     {name:'samsung phone', price:200},
     {name:'wach w3', price:210},
     {name:'wach w4', price:2000},
     {name:'gas lpg', price:1000},
     {name:'gas plg', price:400},
 ]
-function seachProducts(producs,searchText){
-    let seachProduct=[];
-    for(const produc of producs){
-        if(produc.name.includes(searchText)){
-            seachProduct.push(produc);
-        }
+for(const product of producs){
+    if(product.price<2000){
+        continue;
     }
-    return seachProduct;
+    console.log(product);
 }
-console.log(seachProducts(producs,'wach'));
