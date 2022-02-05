3a685b191ae3c440f09462a740af91b22ddfd1e6
masudrana <masudranaacb@gmail.com>
2022-02-05 23:52:13 +0600

    recursion revers

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // recursion
 function recursion(i){
-    if(i>10){
-        return;
+    if(i<=1){
+        return 1;
     }
-    console.log(i);
-    recursion(i+1)
+  return i+recursion(--i);
 }
-recursion(1);
+console.log(recursion(5));
